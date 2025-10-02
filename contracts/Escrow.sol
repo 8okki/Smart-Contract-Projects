635d22a21ea3f8c43bc686701b52be3b06fe6e4a
8okki <thomaskb.lee@gmail.com>
2025-10-02 14:20:10 -0400

    Define modifiers and constructor

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity >=0.8.0 < 0.9.0;
 
 contract Escrow {
 
@@ -8,42 +7,55 @@ contract Escrow {
     address private buyer;
     address private seller;
 
-    uint private targetAmount;
+    uint public targetAmount;
     bool private isFunded;
     bool private isReleased;
-    uint private fee;
 
-    enum Stage {Init, Funded, Released, Completed, Canceled}
+    enum Stage {Init, Funded, Released, Canceled}
+    Stage public currentStage;
 
-    modifier isSeller() {}
-    modifier isBuyer() {}
-    modifier checkStage(Stage _requiredStage) {}
+    modifier isArbiter() {
+        require(msg.sender == arbiter);
+        _;
+    }
+    modifier isSeller() {
+        require(msg.sender == seller);
+        _;
+    }
+    modifier isBuyer() {
+        require(msg.sender == buyer);
+        _;
+    }
+    modifier checkStage(Stage _requiredStage) {
+        require(currentStage == _requiredStage);
+        _;
+    }
 
     /**
     * @param _seller Address of the seller account that offers item to the buyer
     * @param _buyer Address of the buyer account that deposit to this contract
     * @param _targetAmount Amount that needs to be deposited by the buyer
-    * @param _fee Amount of fee that goes to the arbiter (Owner of the escrow)
     */
-    constructor(address _seller, address _buyer, uint _targetAmount, uint _fee) {
-
+    constructor(address _seller, address _buyer, uint _targetAmount) {
+        arbiter = msg.sender;
+        seller = _seller;
+        buyer = _buyer;
+        targetAmount = _targetAmount;
+        currentStage = Stage.Init;
     }
 
-    function deposit() public isBuyer checkStage {
+    function deposit() public isBuyer checkStage(Stage.Init) {
     }
 
-    function confirmDelivery() public isBuyer checkStage {
+    function confirmDelivery() public isBuyer checkStage(Stage.Funded) {
     }
 
-    function refundBuyer() public isArbiter checkStage {
+    function refundBuyer() public isArbiter checkStage(Stage.Funded) {
     }
 
-    function releaseToSeller() public isArbiter checkStage {
+    function releaseToSeller() public isArbiter checkStage(Stage.Funded) {
     }
 
-    function getBalance() public view return (uint balance_) {
-    }
-
-    function getTargetAmount() public view return (uint targetAmount_) {
+    function getBalance() public view returns (uint balance_) {
     }
 }
